6103ebd1132267930c9a3df86a53ea9f6b2e29f4
lmProgramming <mikikubs@gmail.com>
2024-10-05 20:07:20 +0200

    added other pages

--- backend/blog_generator/urls.py
+++ backend/blog_generator/urls.py
@@ -4,4 +4,7 @@ from blog_generator import views
 
 urlpatterns: list[URLPattern] = [
     path('', views.index, name='index'),
+    path('login', views.user_login, name='index'),
+    path('signup', views.user_signup, name='index'),
+    path('logout', views.user_logout, name='index'),
 ]
--- backend/blog_generator/views.py
+++ backend/blog_generator/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request) -> HttpResponse:
     return render(request, 'index.html')
+
+def user_login(request) -> HttpResponse:
+    return render(request, 'login.html')
+
+def user_signup(request) -> HttpResponse:
+    return render(request, 'signup.html')
+
+def user_logout(request) -> HttpResponse:
+    pass
