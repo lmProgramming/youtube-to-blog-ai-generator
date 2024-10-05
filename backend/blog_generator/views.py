5ebc4cb7023ef1beba1ab358a208466cc2f790d7
lmProgramming <mikikubs@gmail.com>
2024-10-05 22:22:27 +0200

    can create account now

--- backend/blog_generator/views.py
+++ backend/blog_generator/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 # Create your views here.
 def index(request) -> HttpResponse:
     return render(request, 'index.html')
@@ -9,6 +10,24 @@ def user_login(request) -> HttpResponse:
     return render(request, 'login.html')
 
 def user_signup(request) -> HttpResponse:
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeatPassword')
+        
+        if password != repeat_password:
+            error_message = 'Passwords do not match'
+            return render(request, 'signup.html', {'error_message': error_message})
+        
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            login(request, user)
+            return redirect('/')
+        except:
+            error_message = 'Username already exists'
+            return render(request, 'signup.html', {'error_message': error_message})            
     return render(request, 'signup.html')
 
 def user_logout(request) -> HttpResponse:
